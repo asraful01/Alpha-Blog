c3331f4fc62e2a3fcacdd636c1675a85c30930d8
ashraful <ashsaas.islam@gmail.com>
2020-03-16 22:54:39 -0400

    extracted redundacy and ready for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,40 +1,23 @@
 class ArticlesController < ApplicationController
+before_action :set_article, only: [:edit,:update, :show,:destroy]
+
 
 def index 
-@articles = Article.all
+    @articles = Article.all
 end
 
-    def new
-        @article=Article.new
-    end
 
-    def edit
-        @article=Article.find(params[:id])
-    end
+def new
+    @article=Article.new
+end
 
-    def update
-        @article= Article.find(params[:id])
-        if @article.update(article_params)
-            flash[:notice] ="Article was successfully updated"
-            redirect_to article_path(@article)
-        else
-            render 'edit'
-        end
-        end
-        def destroy
-@article= Article.find(params[:id])
-@article.destroy
-flash[:notice]= "Article deleted successfully"
+def edit
 
-redirect_to articles_path
+end
 
-        end
-
-
-
-    def create
+def create
          
-         @article=Article.new(article_params)
+     @article=Article.new(article_params)
          if @article.save
             flash[:notice] = "Article was successfully created"
 
@@ -44,11 +27,34 @@ redirect_to articles_path
          end
 
     end
-    def show
-        @article = Article.find(params[:id])
-    end
+    
+def update
+        if @article.update(article_params)
+            flash[:notice] ="Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+end
+
+def destroy
+
+    @article.destroy
+    flash[:notice]= "Article deleted successfully"
+
+    redirect_to articles_path
+
+end
+def show
+
+end
+    
 
     private
+    def set_article
+        @article =Article.find(params[:id])
+
+    end
     def article_params
 params.require(:article).permit(:title, :description)
 
