55c2c104371fb0418877f92474779791283cf530
ashraful <ashsaas.islam@gmail.com>
2020-03-21 17:32:55 -0400

    styling for: error message, show articles, all aritlces

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
 before_action :set_article, only: [:edit,:update, :show,:destroy]
 
 
-def index 
+def index
     @articles = Article.all
 end
 
@@ -16,21 +16,21 @@ def edit
 end
 
 def create
-         
+
      @article=Article.new(article_params)
          if @article.save
-            flash[:notice] = "Article was successfully created"
+          flash[:success] = "Article was successfully created"
 
          redirect_to article_path(@article)
-         else 
+         else
             render 'new'
          end
 
     end
-    
+
 def update
         if @article.update(article_params)
-            flash[:notice] ="Article was successfully updated"
+            flash[:success] ="Article was successfully updated"
             redirect_to article_path(@article)
         else
             render 'edit'
@@ -40,7 +40,7 @@ end
 def destroy
 
     @article.destroy
-    flash[:notice]= "Article deleted successfully"
+    flash[:danger]= "Article deleted successfully"
 
     redirect_to articles_path
 
@@ -48,7 +48,7 @@ end
 def show
 
 end
-    
+
 
     private
     def set_article
