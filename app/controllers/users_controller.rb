ddab7c45630310c64286cb4f23c0c47f8f443e6d
ashraful <ashsaas.islam@gmail.com>
2020-03-24 23:20:06 -0400

    admin setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController <ApplicationController
 
   before_action :set_user, only:[:edit, :update, :show]
-  before_action :require_same_user, only:[:edit, :update]
-
+  before_action :require_same_user, only:[:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
 def index
   @users = User.paginate(page: params[:page],per_page:5)
 end
@@ -49,6 +49,12 @@ def show
 
   @user_articles=@user.articles.paginate(page:params[:page],per_page:5)
 end
+def destroy
+@user= User.find(params[:id])
+@user.destroy
+flash[:danger]= "user and all articles created by user is deleted"
+redirect_to users_path
+end
 
 private
 def user_params
@@ -60,11 +66,20 @@ def set_user
 end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:danger]="You can only edit your own account"
         redirect_to root_path
         end
+
       end
 
+def require_admin
+    if logged_in? and !current_user.admin?
+
+      flash[:danger]="Only admin can perform this action"
+      redirect_to root_path
+
+      end
+end
 
   end
